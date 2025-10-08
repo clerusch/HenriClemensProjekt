81ec81d8ac6ecef8f2052c9ca35bb908a9d70b54
Clemens Schumann <clemens.schumann@fu-berlin.de>
2025-10-08 17:33:51 +0200

    Hab mal mit der error diagram creation angefangen, sollte ich morgen fertig kriegen

--- zxw/src/create_error_diagram.rs
+++ zxw/src/create_error_diagram.rs
@@ -1,45 +1,85 @@
-use quizx::hash_graph::{Graph, VType};
-use quizx::graph::GraphLike;
+use quizx::hash_graph::{Graph, VType, VType::{X, Z, WInput,WOutput,ZBox, B, H}, V};
+use quizx::graph::{VData, GraphLike};
 use quizx::phase::Phase;
 
-fn create_error_diagram (elements: &[(f64, Vec<VType>)], qubits: u8) -> Graph{
+fn create_error_diagram (elements: &[(f64, &'static str)]) -> Graph{
     // Takes a list of tuples, (float probability, paulistring) and 
     // applies that hamiltonian to a diagram of qubits empty lines
-
+    let qubits = elements[0].1.chars().count() as u8;
+    let lelements = elements.len() as f64;
     // create new empty graph
     let mut g = Graph::new();
 
-    // add basic loop with W spider
-    let v1 = g.add_vertex(VType::Z);
-    let v2 = g.add_vertex(VType::Z);
-    let v3 = g.add_vertex(VType::Z);
-    let v4 = g.add_vertex(VType::WInput);
-    let v5 = g.add_vertex(VType::WOutput);
-    let v6 = g.add_vertex_with_phase(VType::X, Phase::from_f64(std::f64::consts::PI));
-    g.add_edge(v1, v3);
-    g.add_edge(v3, v2);
-    g.add_edge(v1, v2);
+    // add left and right boundary spiders
+    for i in 0..qubits {
+        let vdata_l = VData {
+            ty: B,
+            qubit: i as f64 + 3.0,
+            row: 0.0,
+            ..Default::default()
+        };
+        let vdata_r = VData {
+            ty: B,
+            qubit: i as f64 + 3.0,
+            row: lelements*4.0,
+            ..Default::default()
+        };
+        let left = g.add_vertex_with_data(vdata_l);
+        let right = g.add_vertex_with_data(vdata_r);
+        g.add_edge(left, right)
 
-    // connect winput to red pi and winput to woutput?
-    g.add_edge(v4, v5);
-    g.add_edge(v4, v6);
-    
+    }
+    // At this point we have a skeleton of n lines, where n is the amount of qubits
+    // Now next we are going to want a W spider and a paulistring gadget for each in list
+    // This should give us a W spider exactly centered and above the rest of our diagram
+    let wdata = VData {
+        ty: WInput,
+        qubit: 0.0,
+        row: lelements*2.0,
+        ..Default::default()
+    };
+    let w = g.add_vertex_with_data(wdata);
+    // now add the gadgets
+    for i in 0..lelements as u8 {
+        let zboxdata = VData {
+            ty: ZBox,
+            qubit: 1.0,
+            row: i as f64*4.0,
+            phase: Phase::from_f64(elements[i as usize].0),
+            ..Default::default()
+        };
+        let zbox = g.add_vertex_with_data(zboxdata);
+        
+        let hdata = VData {
+            ty: H,
+            qubit: 0.5,
+            row: i as f64*4.0,
+            ..Default::default()
+        };
+        let h = g.add_vertex_with_data(hdata);
+        let xdata = VData {
+            ty: X,
+            qubit: 0.2,
+            row: i as f64*4.0,
+            ..Default::default()
+        };
+        let x = g.add_vertex_with_data(xdata);
+        g.add_edge(w, zbox);
+        g.add_edge(zbox, h);
+        g.add_edge(h, x);
+        //TODO: Add the green spiders on lanes (remove/insert edges)
+        // And add pauli conjugations
 
-
-    for elem in elements {
-        let f = elem.0;
-        let v7 = g.add_vertex_with_phase(VType::ZBox, Phase::from_f64(f));
-        g.add_edge(v5, v7);
-        g.add_edge(v7, v3);
-        for ty in &elem.1 {
-            // add some logic to put stuff on qubit lanes
-            // possibly tad complicated
-        }
     }
     g
 }
 fn fidelity_loop (g:Graph) -> Graph {
     // Add the loops at left and right boundary of diagram to compute fidelity to identity
+    for vertex in g.vertices() {
+        //...
+        // For nice loops will probably have to add a phaseless spider at the bottom to 
+        // connect to
+    }
     g
 }
 
